f477c75373e09cdfb12395d01ee3264f32da30a9
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-12-13 21:12:25 +1100

    simplify the path

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -195,7 +195,7 @@ class _MyAppState extends State<MyApp> {
               Opacity(
                 opacity: .025,
                 child: imageAsset(
-                  'assets/images/arborio_transparent.png',
+                  'images/arborio_transparent.png',
                   fit: BoxFit.scaleDown,
                   width: double.infinity,
                   height: double.infinity,
@@ -456,13 +456,13 @@ class _MyAppState extends State<MyApp> {
                     children: [
                       imageAsset(
                         switch (path.extension(node.data.name).toLowerCase()) {
-                          ('.mp3') => 'assets/images/music.png',
-                          ('.py') => 'assets/images/python.png',
-                          ('.jpg') => 'assets/images/image.png',
-                          ('.png') => 'assets/images/image.png',
-                          ('.dart') => 'assets/images/dart.png',
-                          ('.json') => 'assets/images/json.png',
-                          (_) => 'assets/images/file.png'
+                          ('.mp3') => 'images/music.png',
+                          ('.py') => 'images/python.png',
+                          ('.jpg') => 'images/image.png',
+                          ('.png') => 'images/image.png',
+                          ('.dart') => 'images/dart.png',
+                          ('.json') => 'images/json.png',
+                          (_) => 'images/file.png'
                         },
                         width: 32,
                         height: 32,
@@ -479,7 +479,7 @@ class _MyAppState extends State<MyApp> {
                 RotationTransition(
                   turns: expansionAnimation,
                   child: imageAsset(
-                    'assets/images/folder.png',
+                    'images/folder.png',
                     width: 32,
                     height: 32,
                   ),
